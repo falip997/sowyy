c945393592de9f0e2de24696acc66d43001fd04c
falip997 <fabianaliputra@gmail.com>
2025-06-18 21:42:55 +0700

    Delete sc.js

--- sc.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("forgiveBtn").addEventListener("click", function() {
-  const responseText = document.getElementById("responseText");
-  responseText.textContent = "Aku maafin kamu, tapi jangan ulangi lagi ya ❤️";
-  responseText.style.color = "#ff3366";
-  responseText.style.fontSize = "20px";
-  responseText.style.marginTop = "20px";
-});
